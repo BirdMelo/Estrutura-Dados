e49eeabc95df2bac727fb5f9f10c8322a6d02600
João Pedro de Melo Naves <joao.naves@dinfor.uniceub>
2026-09-15 10:37:06 -0300

    Primeiro Commit

---
+++ lists/vector.py
@@ -0,0 +1,4 @@
+class Vector:
+    def __init__(self,capacity):
+        self.capacity = capacity
+        
---
+++ main.py
@@ -0,0 +1,4 @@
+from .lists.vector import Vector
+from .lists.linked_list import Linked_list
+from .lists.doble_linked_list import Doble_linked_list
+from .lists.circle_linked_list import Circle_linked_list
